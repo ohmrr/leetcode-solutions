de38c7aa1fec5dadcbcf7da21f4399c04c53f427
ohmrr <89240639+ohmrr@users.noreply.github.com>
2025-10-26 20:01:42 -0700

    1929 concatenation of array

---
+++ array/1929_concatenation_of_array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        # ans = []
+        # n = len(nums)
+
+        # for i in range(n * 2):
+        #     ans.append(nums[i % n])
+
+        # return ans
+
+        return nums * 2
