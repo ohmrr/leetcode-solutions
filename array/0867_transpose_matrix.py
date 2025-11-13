042961b6586be58b79d63824a5e3d65e03560d62
ohmrr <89240639+ohmrr@users.noreply.github.com>
2025-11-12 16:26:15 -0800

    fix variables in 867

--- array/0867_transpose_matrix.py
+++ array/0867_transpose_matrix.py
@@ -1,11 +1,11 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        m = len(matrix)
-        n = len(matrix[0])
-        transpose = [[0 for _ in range(m)] for _ in range(n)]
+        n = len(matrix)
+        m = len(matrix[0])
+        transpose = [[0 for _ in range(n)] for _ in range(m)]
 
-        for row in range(n):
-            for col in range(m):
+        for row in range(m):
+            for col in range(n):
                 transpose[row][col] = matrix[col][row]
 
         return transpose
